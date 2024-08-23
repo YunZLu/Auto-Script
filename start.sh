7071fa148a9a336eab9270a6a17c8ada846bfcd4
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-23 21:40:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,51 +3,6 @@
 # Make sure pwd is the directory of the script
 cd "$(dirname "$0")"
 
-if ! command -v go &> /dev/null
-then
-    read -p "还没安装npm，现在安装npm吗? (y/n)" choice
-    case "$choice" in
-      y|Y )
-        echo "npm安装中..."
-        DEBIAN_FRONTEND=noninteractive apt-get install npm -y
-        go version;;
-      n|N )
-        echo "不安装npm，无法启动！"
-        exit;;
-      * )
-        echo "你选了个啥呀？没安装npm，无法启动！"
-        exit;;
-    esac
-fi
-
-if ! command -v go &> /dev/null
-then
-    read -p "还没安装go，现在安装go吗? (y/n)" choice
-    case "$choice" in
-      y|Y )
-        echo "go安装中..."
-        yes | apt update
-        DEBIAN_FRONTEND=noninteractive apt-get install golang -y
-        go version;;
-      n|N )
-        echo "不安装go，无法启动！"
-        exit;;
-      * )
-        echo "你选了个啥呀？没安装go，无法启动！"
-        exit;;
-    esac
-fi
-
-echo "构建前端中..."
-cd /root/one-api/web/default
-npm install
-npm run build
-
-echo "构建后端中..."
-cd ../..
-go mod download
-go build -ldflags "-s -w" -o one-api
-
 echo "One-Api启动中..."
 chmod u+x /root/one-api
 ./one-api --port 3000 --log-dir ./logs
