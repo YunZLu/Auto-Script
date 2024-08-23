7d0a4e5e0079de80b7dbf158d75a695bbb7b8d23
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-24 00:22:16 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,18 +6,18 @@ read -p "确保开了魔法后按回车继续"
 read -p "确定要更新脚本吗? (y/n)" choice
 case "$choice" in
     y|Y )
-      echo "脚本更新中"
-      rm -rf sac.sh
-      curl -O https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/sac.sh
-      if [ ! -f "$current/root/sac.sh" ]; then
-          echo "启动文件下载失败了，换个魔法或者手动下载试试吧"
-      else
-          echo "脚本更新成功！！！"
-      fi
-      ;;
+        echo "脚本更新中..."
+        rm -rf sac.sh
+        curl -O https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/sac.sh
+        if [ ! -f "$current/root/sac.sh" ]; then
+           echo "脚本下载失败了，换个魔法或者手动下载试试吧"
+        else
+           echo "更新脚本成功，正在退出"
+        fi;;
     n|N )
-      echo "不更新，正在退出"
-      exit;;
+        echo "不更新脚本，正在退出"
+        exit;;
     * )
-      echo "你选了个啥呀？到底更不更新了！"
-  esac
+        echo "你选了个啥呀？到底更不更新了！"
+        ;;
+esac
