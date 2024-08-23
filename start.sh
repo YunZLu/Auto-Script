7af34ef7fc84634fbd8c42c7e3c1db9784ccad4e
YunZL <sa8emiu39l@gmail.com>
2024-08-23 16:45:05 +0800

    添加到远程

---
+++ start.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Make sure pwd is the directory of the script
+cd "$(dirname "$0")"
+
+if ! command -v npm &> /dev/null
+then
+    read -p "还没安装npm，现在安装nodejs和npm吗? (y/n)" choice
+    case "$choice" in
+      y|Y )
+        echo "nvm安装中..."
+        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+        source ~/.bashrc
+        nvm install --lts
+        nvm use --lts;;
+      n|N )
+        echo "不安装nvm，无法启动！"
+        exit;;
+      * )
+        echo "你选了个啥呀？没安装nvm，无法启动！"
+        exit;;
+    esac
+fi
+
+if ! command -v go &> /dev/null
+then
+    read -p "还没安装go，现在安装吗? (y/n)" choice
+    case "$choice" in
+      y|Y )
+        echo "安装go中..."
+        DEBIAN_FRONTEND=noninteractive pkg install golang -y
+
+        if command -v go &> /dev/null; then
+            echo "go安装成功"
+            go version                                                
+            else
+            echo "go安装失败，请检查网络后重试。"
+            exit 1
+        fi
+
+      n|N )
+        echo "不安装go，无法启动！"
+        exit;;
+      * )
+        echo "你选了啥呀？没安装go，无法启动！"
+        exit;;
+    esac
+fi
+
+echo "构建前端中..."
+cd one-api/web/default
+npm install
+npm run build
+
+echo "构建后端中..."
+cd ../..
+go mod download
+go build -ldflags "-s -w" -o one-api
+
+echo "One-Api启动中..."
+chmod u+x one-api
+./one-api --port 3000 --log-dir ./logs
---
+++ tisac.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+echo "                                              
+喵喵一键安卓脚本
+作者: hoping喵，坏水秋，瑾年
+来自: Claude2.1先行破限组
+群号: 704819371 / 910524479 / 304690608
+类脑Discord: https://discord.gg/HWNkueX34q
+"
+
+echo -e "\033[0;31m开魔法！开魔法！开魔法！\033[0m\n"
+
+read -p "确保开了魔法后按回车继续"
+
+current=/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu
+
+yes | apt update
+
+yes | apt upgrade
+
+# 安装proot-distro
+DEBIAN_FRONTEND=noninteractive pkg install proot-distro -y
+
+# 创建并安装Ubuntu
+DEBIAN_FRONTEND=noninteractive proot-distro install ubuntu
+
+# Check Ubuntu installed successfully
+ if [ ! -d "$current" ]; then
+   echo "Ubuntu安装失败了，请更换魔法或者手动安装Ubuntu喵~"
+    exit 1
+ fi
+
+    echo "Ubuntu成功安装到Termux"
+
+echo "正在安装相应软件喵~"
+
+DEBIAN_FRONTEND=noninteractive pkg install git vim curl xz-utils npm golang -y
+
+if [ -d "SillyTavern" ]; then
+  cp -r SillyTavern $current/root/
+fi
+
+if [ -d "one-api" ]; then
+  cp -r one-api $current/root/
+fi
+
+cd $current/root
+
+echo "正在为Ubuntu安装node喵~"
+if [ ! -d node-v20.10.0-linux-arm64.tar.xz ]; then
+    curl -O https://nodejs.org/dist/v20.10.0/node-v20.10.0-linux-arm64.tar.xz
+
+tar xf node-v20.10.0-linux-arm64.tar.xz
+
+echo "export PATH=\$PATH:/root/node-v20.10.0-linux-arm64/bin" >>$current/etc/profile
+fi
+
+if [ ! -d "SillyTavern" ]; then
+git clone https://github.com/SillyTavern/SillyTavern
+fi
+
+if [ ! -d "one-api" ]; then
+git clone https://github.com/songquanpeng/one-api.git
+fi
+
+git clone -b test https://github.com/teralomaniac/clewd
+
+echo -e "\033[0;33m本操作仅为破限下载提供方便，所有破限皆为收录，喵喵不具有破限所有权\033[0m"
+read -p "回车进行导入喵~"
+git clone https://github.com/hopingmiao/promot.git st_promot
+if  [ ! -d "st_promot" ]; then
+    echo -e "(*꒦ິ⌓꒦ີ)\n\033[0;33m hoping：因网络波动预设文件下载失败了，更换网络后再试喵~\n\033[0m"
+else
+    cp -r $current/root/st_promot/. $current/root/SillyTavern/public/'OpenAI Settings'/
+    echo -e "\033[0;33m破限已成功导入，安装完毕后启动酒馆即可看到喵~\033[0m"
+fi
+
+curl -O https://raw.githubusercontent.com/hopingmiao/termux_using_Claue/main/sac.sh
+
+if [ ! -f "$current/root/sac.sh" ]; then
+   echo "启动文件下载失败了，换个魔法或者手动下载试试吧"
+   exit
+fi
+
+ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root
+
+echo "bash /root/sac.sh" >>$current/root/.bashrc
+
+echo "proot-distro login ubuntu" >>/data/data/com.termux/files/home/.bashrc
+
+source /data/data/com.termux/files/home/.bashrc
+
+exit
