f1833b7529b7a09cc6bb02ca6695bab518a15030
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-24 01:33:47 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,23 +3,23 @@
 echo -e "\033[0;31m开魔法！开魔法！开魔法！\033[0m\n"
 read -p "确保开了魔法后按回车继续"
 
-read -p "确定要更新脚本吗? (y/n)" choice
+read -p "\033[0;31m确定要更新启动脚本为one-api版吗? (y/n)\033[0;31m" choice
 
 case "$choice" in
     y|Y )
-        echo "脚本更新中..."
+        echo "启动脚本更新中..."
         rm -rf sac.sh
         curl -O https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/sac.sh
         if [ ! -f "$current/root/sac.sh" ]; then
-           echo "脚本下载失败了，换个魔法或者手动下载试试吧"
+           echo "启动脚本下载失败了，换个魔法或者手动下载试试吧"
         else
-           echo "更新脚本成功，正在退出"
+           echo "更新启动脚本成功，正在退出"
            bash /root/sac.sh
         fi;;
     n|N )
-        echo "不更新脚本，正在退出"
+        echo "你选择了不更新启动脚本，正在退出"
         exit;;
     * )
-        echo "你选了个啥呀？到底更不更新了！"
+        echo "m9( ｀д´ )!!!!你选了个啥呀？要不要更新了！"
         ;;
 esac
