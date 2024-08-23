fddec7a96703ee479413c8c89eeb989cdcd33106
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-24 02:46:22 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 echo -e "\033[0;31m开魔法！开魔法！开魔法！\033[0m\n"
 read -p "确保开了魔法后按回车继续"
 
+while :
+do 
 read -p "确定要更新启动脚本为one-api版吗? (y/n)" choice
 
 case "$choice" in
@@ -18,9 +20,11 @@ case "$choice" in
         fi;;
     n|N )
         echo "你选择了不更新启动脚本，正在退出"
-        exit;;
+        break ;;
     * )
         echo "m9( ｀д´ )!!!!你选了个啥呀？要不要更新了！"
-        echo "已退出更新脚本，输入 bash update.sh 可重新进入脚本喵~"
         ;;
 esac
+done 
+echo "已退出喵喵一键脚本，输入 bash sac.sh 可重新进入脚本喵~"
+exit
