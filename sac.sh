13c942b686719be786b2a088e57509b088636f81
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-24 21:22:16 +0800

    Update sac.sh

--- sac.sh
+++ sac.sh
@@ -127,15 +127,8 @@ if [ ! -d "clewd" ]; then
 	echo "clewd下载成功"
 	cd clewd
  
-	{
- 	    bash start.sh
- 	} || {
-	    cd /root 
-	    rm -rf clewd
-	    echo -e "(*꒦ິ⌓꒦ີ)\n\033[0;33m hoping：clewd未部署成功，已帮您删除clewd了喵~\n\033[0m"
-	    continue
-     	}
-     
+ 	bash start.sh
+
         cd /root
 	    if [ ! -f "clewd/config.js" ]; then
 	    rm -rf clewd
