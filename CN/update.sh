5b0e25501cb299890495ace47e46a49335dc4c1e
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-28 21:14:30 +0800

    Update update.sh

--- CN/update.sh
+++ CN/update.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;31m开魔法！开魔法！开魔法！\033[0m\n"
 read -p "确保开了魔法后按回车继续"
 
-read -p "确定要更新启动脚本为one-api版 CN吗? (y/n)" choice
+read -p "确定要更新吗?输入Y更新，按回车键推出喵~" choice
 
 case "$choice" in
     y|Y )
@@ -11,15 +11,12 @@ case "$choice" in
         rm -rf sac.sh
         curl -O https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/CN/sac.sh
         if [ ! -f "$current/root/sac.sh" ]; then
-           echo "启动脚本下载失败了，换个魔法或者手动下载试试吧"
+           echo "更新失败了，正在帮你重试喵~"
         else
-           echo "更新启动脚本成功，正在退出喵..."
+           echo "更新成功，正在退出喵..."
            bash /root/sac.sh
         fi;;
-    n|N )
+   * )
         echo "你选择了不更新启动脚本，正在退出喵..."
         exit;;
-    * )
-        echo "m9( ｀д´ )!!!!你选了个啥呀？要不要更新了！"
-        ;;
 esac
