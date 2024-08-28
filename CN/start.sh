895fd8c34da1605fe84947c004565292074022b5
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-28 19:29:10 +0800

    Update start.sh

--- CN/start.sh
+++ CN/start.sh
@@ -3,43 +3,8 @@
 # Make sure pwd is the directory of the script
 cd "$(dirname "$0")"
 
-if ! command -v go &> /dev/null
-then
-    read -p "还没安装npm，现在安装npm吗? (y/n)" choice
-    case "$choice" in
-      y|Y )
-        echo "npm安装中..."
-        DEBIAN_FRONTEND=noninteractive apt-get install npm -y
-        go version;;
-      n|N )
-        echo "不安装npm，无法启动！"
-        exit;;
-      * )
-        echo "你选了个啥呀？没安装npm，无法启动！"
-        exit;;
-    esac
-fi
-
-if ! command -v go &> /dev/null
-then
-    read -p "还没安装go，现在安装go吗? (y/n)" choice
-    case "$choice" in
-      y|Y )
-        echo "go安装中..."
-        yes | apt update
-        DEBIAN_FRONTEND=noninteractive apt-get install golang -y
-        go version;;
-      n|N )
-        echo "不安装go，无法启动！"
-        exit;;
-      * )
-        echo "你选了个啥呀？没安装go，无法启动！"
-        exit;;
-    esac
-fi
-
 echo "前端构建中..."
-cd /root/one-api/web/default
+cd one-api/web/default
 npm install
 npm run build
 
