3265875f03ceba08c45ff3a01db26e5a7062f083
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-28 17:58:13 +0800

    Update tisac.sh
    修改安装逻辑

--- CN/tisac.sh
+++ CN/tisac.sh
@@ -22,27 +22,10 @@ do
         if ! command -v proot-distro &> /dev/null; then
         echo "proot-distro下载失败了，正在重试中喵~"
         continue
-        else
-        echo "proot-distro安装成功喵~"
         fi
     fi
 done
-
-# 安装git
-while ! command -v git &> /dev/null
-do
-    if ! command -v git &> /dev/null; then
-    echo "检测到你未安装git喵~"
-    echo "正在为你下载git喵~"
-    DEBIAN_FRONTEND=noninteractive pkg install git -y
-        if ! command -v git &> /dev/null; then
-        echo "git下载失败了，正在重试中喵~"
-        continue
-        else
-        echo "git安装成功喵~"
-        fi
-    fi
-done
+echo "proot-distro已安装喵~"
 
 # 加速Ubuntu下载地址
 sed -i 's/https:\/\/github.com/https:\/\/mirror.ghproxy.com\/github.com/g' /data/data/com.termux/files/usr/etc/proot-distro/ubuntu.sh
@@ -64,6 +47,21 @@ do
  
 done
 
+# 安装git
+while ! command -v git &> /dev/null
+do
+    if ! command -v git &> /dev/null; then
+    echo "检测到你未安装git喵~"
+    echo "正在为你下载git喵~"
+    DEBIAN_FRONTEND=noninteractive pkg install git -y
+        if ! command -v git &> /dev/null; then
+        echo "git下载失败了，正在重试中喵~"
+        continue
+        fi
+    fi
+done
+echo "git已安装喵~"
+
 # 安装nodejs
 while ! command -v node &> /dev/null
 do
@@ -74,14 +72,14 @@ do
         if ! command -v node &> /dev/null; then
         echo "nodejs下载失败了，正在重试中喵~"
         continue
-        else
-        echo "nodejs安装成功喵~"
-        #设置npm国内源
-        npm config set registry https://registry.npmmirror.com
         fi
     fi
 done
+echo "nodejs已安装喵~"
+#设置npm国内源
+npm config set registry https://registry.npmmirror.com
 
+#安装go
 while ! command -v go &> /dev/null
 do
     if ! command -v go &> /dev/null; then
@@ -91,14 +89,13 @@ do
         if ! command -v go &> /dev/null; then
         echo "go下载失败了，正在重试中喵~"
         continue
-        else
-        echo "go安装成功喵~"
-        #设置go mod下载使用阿里云加速代理
-        go env -w GO111MODULE=on
-        go env -w GOPROXY=https://mirrors.aliyun.com/goproxy,direct
         fi
     fi
 done
+echo "go已安装喵~"
+#设置go mod下载使用阿里云加速代理
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://mirrors.aliyun.com/goproxy,direct
 
 cd $current/root
 
