17ed7e2c9a5015094a9041bcc02dda59e564cd65
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-28 19:57:46 +0800

    Update start.sh

--- CN/start.sh
+++ CN/start.sh
@@ -3,17 +3,17 @@
 # Make sure pwd is the directory of the script
 cd "$(dirname "$0")"
 
-echo "前端构建中..."
+echo "前端构建中喵..."
 cd web/default
 npm install
 npm run build
 
-echo "后端构建中..."
+echo "后端构建中喵..."
 cd ../..
-echo "第一次下载依赖包会比较慢，耐心等待..."
+echo "第一次下载依赖包会比较慢，耐心等待喵..."
 go mod download
 go build -ldflags "-s -w" -o one-api
 
-echo "One-Api启动中..."
+echo "One-Api启动中喵..."
 chmod u+x one-api
 ./one-api --port 3000 --log-dir ./logs
