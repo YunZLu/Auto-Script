0d40ddb6d65d5f23bf02f1277ec31be83dc057b4
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 18:21:35 +0800

    Update only_oneapi_sac.sh

--- CN/only_oneapi_sac.sh
+++ CN/only_oneapi_sac.sh
@@ -4,6 +4,62 @@ echo -e "
 \033[0;32m作者：瑾年\033[0m
 \033[0;32m来自：头脑风暴搞起来\033[0m
 "
+
+echo -e "\033[0;33m喵喵正在帮你检查系统环境中，请稍等一下喵~\n\033[0m"
+
+# 安装git
+while ! command -v git &> /dev/null
+do
+    if ! command -v git &> /dev/null; then
+    echo -e "\033[0;31m检测到你未安装git喵~\n\033[0m"
+    echo -e "\033[0;33m正在为你下载git，请稍等一下喵~\n\033[0m"
+    DEBIAN_FRONTEND=noninteractive pkg install git -y
+        if ! command -v git &> /dev/null; then
+        echo -e "\033[0;31mgit下载失败了，正在重试中，请稍等一下喵喵~\n\033[0m"
+	sleep 2
+        continue
+        fi
+    fi
+done
+echo -e "\n\033[0;32mgit已安装喵~\n\033[0m"
+
+# 安装nodejs
+while ! command -v node &> /dev/null
+do
+    if ! command -v node &> /dev/null; then
+    echo -e "\033[0;31m检测到你未安装nodejs喵~\n\033[0m"
+    echo -e "\033[0;33m正在为你下载nodejs，请稍等一下喵~\n\033[0m"
+    DEBIAN_FRONTEND=noninteractive pkg install nodejs-lts -y
+        if ! command -v node &> /dev/null; then
+        echo -e "\033[0;31mnodejs下载失败了，正在重试中，请稍等一下喵~\n\033[0m"
+	sleep 2
+        continue
+        fi
+    fi
+done
+echo -e "\n\033[0;32mnodejs已安装喵~\n\033[0m"
+#设置npm国内源
+npm config set registry https://registry.npmmirror.com
+
+#安装go
+while ! command -v go &> /dev/null
+do
+    if ! command -v go &> /dev/null; then
+    echo -e "\033[0;31m检测到你未安装go喵~\n\033[0m"
+    echo -e "\033[0;33m正在为你下载go，请稍等一下喵~\n\033[0m"
+    DEBIAN_FRONTEND=noninteractive pkg install golang -y
+        if ! command -v go &> /dev/null; then
+        echo -e "\033[0;31mgo下载失败了，正在重试中，请稍等一下喵~\n\033[0m"
+	sleep 2
+        continue
+        fi
+    fi
+done
+echo -e "\n\033[0;32mgo已安装喵~\033[0m\n"
+#设置go mod下载使用阿里云加速代理
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://mirrors.aliyun.com/goproxy,direct
+
 while :
 do 
 echo -e "\033[0;36m喵喵~让你选一个执行（输入数字即可），懂了喵？\033[0m
