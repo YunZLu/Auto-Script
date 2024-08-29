4b43281bc707e7a943e2ba44eb21dd5deee9348d
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 11:43:11 +0800

    Update start.sh

--- CN/start.sh
+++ CN/start.sh
@@ -9,12 +9,12 @@ npm install
 npm run build
 echo -e "\033[0;32m前端构建成功喵~\n\033[0m"
 
-echo -e "\033[0;33m后端构建中，请稍等一下喵...\n\033[0m"
+echo -e "\n\033[0;33m后端构建中，请稍等一下喵...\n\033[0m"
 cd ../..
 echo -e "\033[0;33m如果是第一次下载依赖包，会比较慢，请耐心等待喵~\n\033[0m"
 go mod download
 go build -ldflags "-s -w" -o one-api
-echo -e "\033[0;32m后端构建中成功喵~\n\033[0m"
+echo -e "\033[0;32m后端构建成功喵~\n\033[0m"
 
 echo -e "\033[0;33mOne-Api启动中，请稍等一下喵~\n\033[0m"
 chmod u+x one-api
