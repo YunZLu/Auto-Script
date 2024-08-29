c791a72922f673a5a11260d6af59ef2fb9891128
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 13:53:46 +0800

    Update sac.sh

--- CN/sac.sh
+++ CN/sac.sh
@@ -83,9 +83,10 @@ echo -e "\n\033[0;31已更换国内代理，并成功升级软件包喵~\n\033[0
 	else
         echo -e "\n\033[0;32mgo安装成功喵~\033[0m"
 	#重装ca-certificates解决go mod X509问题
- 	echo -e "\n\033[0;33mgo正在重装ca-certificates，请稍等一下喵~\033[0m"
+ 	echo -e "\n\033[0;33m正在重装ca-certificates，请稍等一下喵~\033[0m"
         apt-get install --reinstall ca-certificates -y
         yes | apt-get update
+ 	echo -e "\n\033[0;32mca-certificates重装成功喵~\033[0m"
         fi
     fi
 done
