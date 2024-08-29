e4612625e8a9ed1dc0cdf94407d76705d06f32ba
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 10:19:25 +0800

    Update only_oneapi_sac.sh

--- CN/only_oneapi_sac.sh
+++ CN/only_oneapi_sac.sh
@@ -1,8 +1,8 @@
 # 主菜单
 echo -e "                                              
-喵喵一键脚本(one-api CN版)
-作者：瑾年
-来自：头脑风暴搞起来
+\033[0;32m喵喵一键脚本(one-api CN版)\033[0m
+\033[0;32m作者：瑾年\033[0m
+\033[0;32m来自：头脑风暴搞起来\033[0m
 "
 while :
 do 
@@ -25,25 +25,26 @@ echo -e "\033[0;36m喵喵~让你选一个执行（输入数字即可），懂了
 	    ps -ef | grep one-api | awk '{print$2}' | xargs kill -9
             cd one-api
             bash start.sh
-            echo "One-Api已关闭, 即将返回主菜单喵~"
+            echo -e "\033[0mOne-Api已关闭, 即将返回主菜单喵~\033[0m"
             cd ../
             ;; 
         2) 
             #安装酒馆
             echo ""> .bashrc
-            echo -e "\033[0;36m确定要安装酒馆喵?\033[0m\n"
-	    read -p "输入Y安装酒馆，或者按回车键返回喵~" choice
+            echo -e "\033[0;35m确定要安装酒馆喵?\033[0m\n"
+	    echo -e "\033[0;35m输入Y安装酒馆，或者按回车键返回喵~\033[0m\n"
+     	    read -n 1 choice
             case "$choice" in
    	    y|Y )
             curl -O https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/CN/tisac.sh && chmod +x tisac.sh && ./tisac.sh;;
 	    * )
-            echo "你已选择不安装酒馆喵~";;
+            echo -e "\033[0;32m你已选择不安装酒馆喵~\033[0m\n";;
 	    esac
             ;; 
         *) 
-            echo -e "m9( ｀д´ )!!!! \n\033[0;36m坏猫猫居然不听话，存心和我喵喵~过不去是吧？\033[0m\n"
+            echo -e "m9( ｀д´ )!!!! \n\033[0;31m坏猫猫居然不听话，存心和我喵喵~过不去是吧？\033[0m\n"
             ;;
     esac
 done 
-echo "已退出喵喵一键脚本，输入 bash only_oneapi_sac.sh 可重新进入脚本喵~"
+echo -e "\033[0;32m已退出喵喵一键脚本，输入 bash only_oneapi_sac.sh 可重新进入脚本喵~\033[0m\n"
 exit
