cb745a3f4ee322faf3e34862b5e7831864033598
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 13:11:29 +0800

    Update sac.sh

--- CN/sac.sh
+++ CN/sac.sh
@@ -41,7 +41,7 @@ do
     
 echo -e "\033[0;33m喵喵正在帮你选择国内代理中，请稍等一下喵~\n\033[0m"
      bash <(curl -sSL https://linuxmirrors.cn/main.sh) << eof
-    2
+    6
 eof
     yes | apt update
 echo -e "\n\033[0;31已更换国内代理，并成功升级软件包喵~\n\033[0m"
