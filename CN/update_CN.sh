6175f71b9cc2aa4b73e6da81a7e8d43d7c973d1d
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-08-29 11:00:00 +0800

    Update update_CN.sh

--- CN/update_CN.sh
+++ CN/update_CN.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-read -p "确定要更新吗?输入Y更新，或者按回车键退出喵~" choice
-
+echo -e "\033[0;35m确定要更新吗?输入Y更新，或者按回车键退出喵~\n\033[0m"
+read -n 1 choice
 case "$choice" in
     y|Y )
         echo "启动脚本更新中..."
         rm -rf sac.sh
         curl -O https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/CN/sac.sh
         if [ ! -f "$current/root/sac.sh" ]; then
-           echo "更新失败了，正在帮你重试喵~"
+           echo -e "\033[0;31m更新失败了，正在帮你重试喵~\n\033[0m"
         else
-           echo "更新成功，正在退出喵..."
+           echo -e "\033[0;32m更新成功，正在退出喵...\n\033[0m"
            bash /root/sac.sh
         fi;;
    * )
-        echo "你选择了不更新启动脚本，正在退出喵..."
+        echo -e "\033[0;32m你选择了不更新启动脚本，正在退出喵...\n\033[0m"
         exit;;
 esac
