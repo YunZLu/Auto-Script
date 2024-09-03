1b6238c72df3cc63de29e8475200254b4e8f55cd
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-09-03 09:43:17 +0800

    Update sac.sh

--- QC_CN/sac.sh
+++ QC_CN/sac.sh
@@ -73,6 +73,7 @@ eof
     tar xf node-v20.10.0-linux-arm64.tar.xz
     echo "export PATH=\$PATH:/root/node-v20.10.0-linux-arm64/bin" >>/etc/profile
     source /etc/profile
+    echo "source /etc/profile" >>/root/.bashrc
         if ! command -v node &> /dev/null; then
         echo -e "\033[0;31mnodejs下载失败了，正在重试中，请稍等一下喵~\n\033[0m"
 	sleep 2
