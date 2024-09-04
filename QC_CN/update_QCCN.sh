51df09a14a5f663721244705d0a51c154d87877a
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-09-04 23:31:10 +0800

    Update update_QCCN.sh

--- QC_CN/update_QCCN.sh
+++ QC_CN/update_QCCN.sh
@@ -5,14 +5,14 @@ read -s -n 1 choice
 case "$choice" in
     y|Y )
         echo -e "\n\033[0;33m启动脚本更新中，请稍等一下喵...\n\033[0m"
-        rm -rf sac.sh
+        rm -rf /root/sac.sh
         curl -O https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/QC_CN/sac.sh
         if [ ! -f "$current/root/sac.sh" ]; then
            echo -e "\n\033[0;31m更新失败了，正在帮你重试，请稍等一下喵~\n\033[0m"
         else
            echo -e "\n\033[0;32m更新成功，正在重新启动脚本喵...\n\033[0m"
-           chmod 777 ./sac.sh
-           exec ./sac.sh
+           chmod 777 /root/sac.sh
+           exec /root/sac.sh
         fi;;
    * )
         echo -e "\n\n\033[0;33m你选择了不更新启动脚本，正在重新启动脚本喵...\n\033[0m"
