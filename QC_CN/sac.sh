9d497b9916ff9cc0c718f2070e4d11dc9b18439e
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-09-04 16:25:36 +0800

    Update sac.sh

--- QC_CN/sac.sh
+++ QC_CN/sac.sh
@@ -1278,7 +1278,7 @@ do
 	  		esac
 			;;
 		    2)
-	            if [ ! $(ls -1 /opt/QQ/resources/app/app_launcher/napcat/config/ | awk -F'_' '{print $2}' | awk -F'.' '{print $1}' | awk '!a[$0]++{print}'| awk NF | awk NR==1) ]; then
+	            if [ ! $(ls -1 /opt/QQ/resources/app/app_launcher/napcat/config/ | awk -F'_' '{print $2}' | awk -F'.' '{print $1}' | awk '!a[$0]++{print}' | awk 'NF{a++;print "\033[0;33m"a"\033[0m""\033[0;33m.\033[0m","\033[0;33m"$0"\033[0m\n";next}1) ]; then
 		    		echo -e "\n\033[0;31m没有登录过的QQ号，请先扫码添加QQ号喵~\033[0m"
 	         		echo -e "\033[0;31m请按任意键返回喵~\033[0m"
 	     			read -s -n 1
@@ -1288,7 +1288,7 @@ do
 		    		echo -e "\033[0;36m请输入数字登录对应的QQ号喵~\033[0m"
 		    		read -s -n 1 QQchose
 					#QQnumber=$(ls -1 /opt/QQ/resources/app/app_launcher/napcat/config/ | awk -F'_' '{print $2}' | awk -F'.' '{print $1}' | awk '!a[$0]++{print}'| awk NF | awk -v QQchose=$QQchose NR==$QQchose)
-					if [ QQnumber=$(ls -1 /opt/QQ/resources/app/app_launcher/napcat/config/ | awk -F'_' '{print $2}' | awk -F'.' '{print $1}' | awk '!a[$0]++{print}'| awk NF | awk -v QQchose=$QQchose NR==$QQchose) ]; then
+					if [ QQnumber=$(ls -1 /opt/QQ/resources/app/app_launcher/napcat/config/ | awk -F'_' '{print $2}' | awk -F'。' '{print $1}' | awk '!a[$0]++{print}'| awk NF | awk -v QQchose=$QQchose NR==$QQchose) ]; then
 	                	echo -e "\n\033[0;36m你确定要登录以下QQ号喵？(y|N)\033[0m"
 						echo -e "\n\033[0;33m$QQnumber\033[0m"
 		  			    read -s -n 1 chose
