aa0c004dd68ad4753776b8c84e8edd58d30097f1
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-09-04 12:06:48 +0800

    Update sac.sh

--- QC_CN/sac.sh
+++ QC_CN/sac.sh
@@ -71,9 +71,9 @@ eof
     echo -e "\033[0;33m正在为你下载nodejs，请稍等一下喵~\n\033[0m"
     curl -O https://cdn.npmmirror.com/binaries/node/v20.10.0/node-v20.10.0-linux-arm64.tar.xz
     tar xf node-v20.10.0-linux-arm64.tar.xz
-    echo "export PATH=\$PATH:/root/node-v20.10.0-linux-arm64/bin" >>/etc/profile
-    source /etc/profile
-    echo "source /etc/profile" >>/root/.bashrc
+    echo "export PATH=\$PATH:/root/node-v20.10.0-linux-arm64/bin" >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/profile
+    source /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/profile
+    echo "source /etc/profile" >>/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
         if ! command -v node &> /dev/null; then
         echo -e "\033[0;31mnodejs下载失败了，正在重试中，请稍等一下喵~\n\033[0m"
 	sleep 2
