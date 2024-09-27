7c6ed2d2bf3f6a2c22af2d739950fbd82b9c35fb
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-09-28 01:12:48 +0800

    Update update_CN.sh

--- CN/update_CN.sh
+++ CN/update_CN.sh
@@ -6,7 +6,7 @@ case "$choice" in
     y|Y )
         echo -e "\n\033[0;35m启动脚本更新中，请稍等一下喵...\n\033[0m"
         rm -rf sac.sh
-        curl -O https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/CN/sac.sh
+        curl -O -L https://mirror.ghproxy.com/https://raw.githubusercontent.com/YunZLu/termux_using_openai/main/CN/sac.sh
         if [ ! -f "$current/root/sac.sh" ]; then
            echo -e "\033[0;31m更新失败了，正在帮你重试，请稍等一下喵~\n\033[0m"
         else
